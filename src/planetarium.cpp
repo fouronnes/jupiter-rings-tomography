91d924df7f165d8f3b9f2f8ce4d5d812fef6e868
Victor Poughon <victor@pghn.eu>
2014-11-10 11:45:59 +0100

    added draw_star function (gaussian point spread)

--- src/planetarium.cpp
+++ src/planetarium.cpp
@@ -8,11 +8,58 @@
 
 typedef unsigned char uchar;
 
+// The value of a gaussian point spread function at a given distance
+float psf_gaussian(float distance) {
+    return 1.0 * exp(-(distance*distance) / 5.0);
+}
+
+// Draw a star at given screen coordinates
+// Uses a gaussian point spread function
+void draw_star(cv::Mat image, float star_x, float star_y) {
+
+    // Max size of the spread to one side in pixels
+    // e.g. 1 means the square of pixels modified will be 3x3
+    const int spread_size = 4;
+
+    // Closest discrete pixel to the star location
+    const int center_x = round(star_x);
+    const int center_y = round(star_y);
+    
+    // For each pixel around the star
+    for (int x = center_x - spread_size; x <= center_x + spread_size; x++) {
+        for (int y = center_y - spread_size; y <= center_y + spread_size; y++) {
+
+            // Get the point spread function value at that distance from the star
+            float distance = sqrt(pow(star_x - x, 2) + pow(star_y - y, 2));
+            image.at<float>(x,y) += psf_gaussian(distance);
+        }
+    }
+}
+
 int main() {
     cv::namedWindow("Planetarium", cv::WINDOW_AUTOSIZE);
 
-    while(cvWaitKey(5) != 27) {
+    cv::Mat image(600, 800, CV_32FC1);
 
+    draw_star(image, 100, 100.0);
+    draw_star(image, 110, 100.1);
+    draw_star(image, 120, 100.2);
+    draw_star(image, 130, 100.3);
+    draw_star(image, 140, 100.4);
+    draw_star(image, 150, 100.4);
+    draw_star(image, 160, 100.6);
+    draw_star(image, 170, 100.7);
+    draw_star(image, 180, 100.8);
+    draw_star(image, 190, 100.9);
+    draw_star(image, 200, 101.0);
+
+    // Threshold to 1.0
+    std::cout << image.at<float>(100, 100) << std::endl;
+    cv::threshold(image, image, 1.0, 0.0, cv::THRESH_TRUNC);
+    std::cout << image.at<float>(100, 100) << std::endl;
+
+    while(cvWaitKey(5) != 27) {
+        cv::imshow("Planetarium", image);
     }
 
     return 0;
