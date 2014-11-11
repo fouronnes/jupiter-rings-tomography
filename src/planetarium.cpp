d0fd5ee9e3878758a729da33b81e60e6bab73d1b
Victor Poughon <victor@pghn.eu>
2014-11-11 08:46:48 +0100

    introduced planetarium object

--- src/planetarium.cpp
+++ src/planetarium.cpp
@@ -47,68 +47,77 @@ cv::Mat load_catalog(const char* filename) {
     return catalog;
 }
 
-// The value of a gaussian point spread function at a given distance
-float psf_gaussian(float distance) {
-    // Amplitude and std
-    return 1.0 * exp(-(distance*distance) / 1.0);
-}
+struct planetarium {
+    // Point spread function amplitude
+    double psf_amplitude;
 
-// Draw a star at given screen coordinates
-// Uses a gaussian point spread function
-void draw_star(cv::Mat image, float star_x, float star_y) {
+    // Point spread function b parameter
+    double psf_sigma;
 
     // Max size of the spread to one side in pixels
     // e.g. 1 means the square of pixels modified will be 3x3
-    const int spread_size = 5;
+    double psf_spread_size;
 
-    // Closest discrete pixel to the star location
-    const int center_x = round(star_x);
-    const int center_y = round(star_y);
-    
-    // For each pixel around the star
-    for (int x = center_x - spread_size; x <= center_x + spread_size; x++) {
-        for (int y = center_y - spread_size; y <= center_y + spread_size; y++) {
+    // Screen
+    double screen_distance;
+    double screen_width;
+    double screen_height;
+    double screen_horizontal_pixel_size;
+    double screen_vertical_pixel_size;
 
-            // Get the point spread function value at that distance from the star
-            float distance = sqrt(pow(star_x - x, 2) + pow(star_y - y, 2));
+    // The value of a gaussian point spread function at a given distance
+    float psf_gaussian(float distance) {
+        return psf_amplitude * exp(-(distance*distance) / psf_sigma);
+    }
 
-            // If within bounds, add it to current value
-            if (y > 0 && x > 0 && y < image.rows && x < image.cols) {
-                image.at<float>(y,x) += psf_gaussian(distance);
+    // Draw a star at given screen coordinates
+    // Uses a gaussian point spread function
+    void draw_star(cv::Mat image, float star_x, float star_y) {
+        // Closest discrete pixel to the star location
+        const int center_x = round(star_x);
+        const int center_y = round(star_y);
+
+        // For each pixel around the star
+        for (int x = center_x - psf_spread_size; x <= center_x + psf_spread_size; x++) {
+            for (int y = center_y - psf_spread_size; y <= center_y + psf_spread_size; y++) {
+
+                // Get the point spread function value at that distance from the star
+                float distance = sqrt(pow(star_x - x, 2) + pow(star_y - y, 2));
+
+                // If within bounds, add it to current value
+                if (y > 0 && x > 0 && y < image.rows && x < image.cols) {
+                    image.at<float>(y,x) += psf_gaussian(distance);
+                }
             }
         }
     }
-}
 
-void draw_visible_stars(cv::Mat image, cv::Mat catalog) {
-    const double screen_distance = .3;
-    const double screen_width = 800;
-    const double screen_height = 600;
-    const double screen_horizontal_pixel_size = 0.0002 ;
-    const double screen_vertical_pixel_size = 0.0002;
+    void draw_visible_stars(cv::Mat image, cv::Mat catalog) {
+        // For each entry in the catalog
+        for (int i = 0; i < catalog.rows; i++) {
+            const double ra = catalog.at<float>(i, 1);
+            const double de = catalog.at<float>(i, 2);
 
-    for (int i = 0; i < catalog.rows; i++) {
-        double ra = catalog.at<float>(i, 1);
-        double de = catalog.at<float>(i, 2);
+            const double max_ra = std::atan((screen_width * screen_horizontal_pixel_size) / (2*screen_distance));
+            const double max_de = std::atan((screen_height * screen_vertical_pixel_size) / (2*screen_distance));
 
-        double max_ra = std::atan((screen_width * screen_horizontal_pixel_size) / (2*screen_distance));
-        double max_de = std::atan((screen_height * screen_vertical_pixel_size) / (2*screen_distance));
+            // If star is visible
+            // RA is in [0;2pi] but DE is in [-pi;pi]
+            if (ra - M_PI > -max_ra && ra - M_PI < max_ra && de > -max_de && de < max_de) {
+                const double x = screen_distance * tan(ra);
+                const double y = screen_distance * tan(de);
 
-        // RA is in [0;2pi] but DE is in [-pi;pi]
-        if (ra - M_PI > -max_ra && ra - M_PI < max_ra && de > -max_de && de < max_de) {
-            double x = screen_distance * tan(ra);
-            double y = screen_distance * tan(de);
+                // Meters to pixel
+                const int x_screen = round(-x / screen_horizontal_pixel_size + screen_width / 2);
+                const int y_screen = round(-y / screen_vertical_pixel_size + screen_height / 2);
 
-            // Meters to pixel
-            int x_screen = round(-x / screen_horizontal_pixel_size + screen_width / 2);
-            int y_screen = round(-y / screen_vertical_pixel_size + screen_height / 2);
-
-            if (x_screen > 0 && x_screen < screen_width && y_screen > 0 && y_screen < screen_height) {
-                draw_star(image, x_screen, y_screen);
+                if (x_screen > 0 && x_screen < screen_width && y_screen > 0 && y_screen < screen_height) {
+                    draw_star(image, x_screen, y_screen);
+                }
             }
         }
     }
-}
+};
 
 int main() {
     cv::namedWindow("Planetarium", cv::WINDOW_AUTOSIZE);
@@ -116,10 +125,22 @@ int main() {
     // Black background
     cv::Mat image(600, 800, CV_32FC1);
 
+    planetarium wall;
+
+    wall.psf_amplitude = 1.0;
+    wall.psf_sigma = 1.0;
+    wall.psf_spread_size = 5;
+
+    wall.screen_distance = .3;
+    wall.screen_width = 800;
+    wall.screen_height = 600;
+    wall.screen_horizontal_pixel_size = 0.0002 ;
+    wall.screen_vertical_pixel_size = 0.0002;
+
     // Load star catalog
     cv::Mat catalog = load_catalog("../hip5.tsv");
 
-    draw_visible_stars(image, catalog);
+    wall.draw_visible_stars(image, catalog);
 
     // Threshold to 1.0
     cv::threshold(image, image, 1.0, 0.0, cv::THRESH_TRUNC);
